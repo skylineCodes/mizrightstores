fdfaa5f3201fdc1bffc1b1a0a97b0acd1be6f8b1
SkylineCodes <onakoyak@gmail.com>
2021-07-09 09:41:37 +0100

    Added Procfile for heroku

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    // res.sendFile('index.html', { root: __dirname });
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    // res.sendFile(path.join('build', 'index.html'));
+  });
+}
+
+app.listen(process.env.PORT || 8080);
